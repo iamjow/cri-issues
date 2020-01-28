8db3b845cf757a292d71dfbeb2cdb38300c005a4
iamjowin <jowin@qstrike.com>
2020-01-28 15:42:43 +0800

    initial cri issues

---
+++ config.py
@@ -0,0 +1,23 @@
+from requests.auth import HTTPBasicAuth
+
+username = "jowin@qstrike.com"
+password = "nH484g7DFwDAMUoRunufFC3D"
+auth = HTTPBasicAuth(username, password)
+
+maxResult = 100
+startAt = 0
+
+styleId = "customfield_10440"
+server = "qstrike.atlassian.net"
+project_key = "CRI"
+fields = "fields=key,summary," + styleId + ",status,labels"
+max_results = "maxResults={}".format(maxResult)
+start_at = "startAt={}".format(startAt)
+url = "https://" + server + "/rest/api/2/search?jql=project=" + project_key + "&" + fields + "&" + max_results + "&" + start_at + ""
+
+url_issue_link = "https://qstrike.atlassian.net/browse/"
+
+ws = "C:\\Users\\Public\\cri_issues.xlsx"
+
+
+
---
+++ main.py
@@ -0,0 +1,46 @@
+from openpyxl import Workbook
+
+if __name__ == '__main__':
+    import main_index
+    import config
+    import json
+    import requests
+
+    auth = config.auth
+    url = config.url
+    response = requests.request("GET", url, auth=auth)
+    data_issues = json.loads(response.text)
+
+    url_issue_link = config.url_issue_link
+
+    worksheet = config.ws
+
+    labels = []
+    item_list1 = []
+    item_list2 = []
+
+    status = 'Done'
+
+    id_style = config.styleId
+    style_id = id_style
+
+    workbook = Workbook()
+    work_sheet1 = workbook.active
+    work_sheet2 = workbook.create_sheet('Sheet1')
+
+    def getMainIndexData():
+        main_data = main_index.__fetchDataFromJira__(data_issues,
+                                                     worksheet,
+                                                     item_list1,
+                                                     item_list2,
+                                                     labels,
+                                                     style_id,
+                                                     status,
+                                                     url_issue_link,
+                                                     workbook,
+                                                     work_sheet1,
+                                                     work_sheet2)
+        main_data.displayData()
+        main_data.displayStyleSheet()
+
+getMainIndexData()
